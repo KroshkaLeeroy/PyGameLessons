cd8978a73bd56a83d0e189433992c0ea9c2f5861
vasil <va>
2022-11-30 10:30:12 +0300

    Новая игра змейка создание базы

---
+++ Snake/main.py
@@ -0,0 +1,22 @@
+import sys
+import pygame
+
+pygame.init()
+
+clock = pygame.time.Clock()
+
+screen_width = 1280
+screen_height = 960
+
+screen = pygame.display.set_mode((screen_width,screen_height))
+pygame.display.set_caption('Snake')
+
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update() # Или .flip()
