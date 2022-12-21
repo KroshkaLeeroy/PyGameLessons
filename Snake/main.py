d251a0c5fb48c7df775b75c77c60c1690fee34d9
vasil <va>
2022-12-21 13:23:21 +0300

    1 Змейка, создание функции движения 2 Управление игроком

--- Snake/main.py
+++ Snake/main.py
@@ -4,29 +4,32 @@ from pygame import Vector2
 import random
 
 
-# 3 создание класса змеи, для хранения информации о координатах ее тела
 class SNAKE:
     def __init__(self):
-        # 4 Создание 3х блоков тела змеи в начале игры
-        self.body = [Vector2(5,10), Vector2(6,10), Vector2(7,10)]
+        self.body = [Vector2(5, 10), Vector2(6, 10), Vector2(7, 10)]
+        self.direction = Vector2(1, 0)
 
     def draw_snake(self):
-        # 5 по очереди отрисовываем блоки тела змеи
         for block in self.body:
-            # 6 Повторяемся так же как и с отрисовкой фрукта
             block_rect = pygame.Rect(block.x * cell_size, block.y * cell_size, cell_size, cell_size)
-            pygame.draw.rect(screen, (183,111,122), block_rect)
+            pygame.draw.rect(screen, (183, 111, 122), block_rect)
+
+    # 1 Создаем функцию движения змеи.
+    # Движение будет осуществляться путем смещения блоков тела змеи по вектору ее движения
+    def move_snake(self):
+        body_copy = self.body[:-1]  # Копируем все тело змеи за исключением последнего блока.
+        body_copy.insert(0, body_copy[0] + self.direction)  # Вставляем в копию тела змеи, на место первого блока, блок
+        # который будем изменять свои координаты отталкиваясь от вектора направления self.direction
+        self.body = body_copy[:]
 
 
 class FRUIT:
     def __init__(self):
-        # 2 Используя рандом размещаем Х в случайной ячейке карты, -1 потому что 20 ячейка находится за экраном
-        self.x = random.randint(0,cell_number - 1)
-        self.y = random.randint(0,cell_number - 1)
+        self.x = random.randint(0, cell_number - 1)
+        self.y = random.randint(0, cell_number - 1)
 
         self.pos = Vector2(self.x, self.y)
 
-    # 1 Поправляем размещение фрукта, с созданием иллюзии сетки по которой все ставится и двигается
     def draw_fruit(self):
         fruit_rect = pygame.Rect(self.pos.x * cell_size, self.pos.y * cell_size, cell_size, cell_size)
         pygame.draw.rect(screen, (126, 166, 114), fruit_rect)
@@ -45,20 +48,36 @@ pygame.display.set_caption('Snake')
 
 
 fruit = FRUIT()
-# 6 Создание объекта для класса змеи
 snake = SNAKE()
+# 2 Для создания движение змеи нам необходимо передвигать ее блоки с равным промежутком времени
+# для этого создаем кастомный ивент который будет вызываться каждые 150 миллисекунд по таймеру ниже
+# в зависимости от цифры будет меняться скорость передвижения змеи
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE, 150)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        # 3 Теперь мы описываем, что нам необходимо делать в момент когда срабатывает наш таймер в списке событий
+        if event.type == SCREEN_UPDATE:
+            snake.move_snake()
+        # 4 Теперь наконец можно прикручивать управление кнопками
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                # Изменяем вектор движения змеи
+                snake.direction = Vector2(0, -1)
+            if event.key == pygame.K_DOWN:
+                snake.direction = Vector2(0, 1)
+            if event.key == pygame.K_LEFT:
+                snake.direction = Vector2(-1, 0)
+            if event.key == pygame.K_RIGHT:
+                snake.direction = Vector2(1, 0)
 
     screen.fill((175, 215, 70))
 
-
     fruit.draw_fruit()
-    # 7 Физическая отрисовка змеи
     snake.draw_snake()
 
     pygame.display.update()
