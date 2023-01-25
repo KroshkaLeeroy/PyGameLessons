8f31818250dcb145855cf65b1b13cc39f2bd0a19
vasil <va>
2023-01-25 12:41:32 +0300

    Змейка 1 Добавление файлов для отрисовки змеи. 2 Создание метода отслеживания позиции головы. 3 Физическая отрисовка головы.

--- Snake/main.py
+++ Snake/main.py
@@ -6,15 +6,64 @@ import random
 
 class SNAKE:
     def __init__(self):
-        # 5
         self.body = [Vector2(5, 10), Vector2(4, 10), Vector2(3, 10)]
         self.direction = Vector2(1, 0)
         self.new_block = False
 
+        # 1 Загрузка всех возможных спрайтов змейки
+        self.head_up = pygame.image.load('Graphics/head_up.png').convert_alpha()
+        self.head_down = pygame.image.load('Graphics/head_down.png').convert_alpha()
+        self.head_right = pygame.image.load('Graphics/head_right.png').convert_alpha()
+        self.head_left = pygame.image.load('Graphics/head_left.png').convert_alpha()
+
+        self.tail_up = pygame.image.load('Graphics/tail_up.png').convert_alpha()
+        self.tail_down = pygame.image.load('Graphics/tail_down.png').convert_alpha()
+        self.tail_right = pygame.image.load('Graphics/tail_right.png').convert_alpha()
+        self.tail_left = pygame.image.load('Graphics/tail_left.png').convert_alpha()
+
+        self.body_vertical = pygame.image.load('Graphics/body_vertical.png').convert_alpha()
+        self.body_horizontal = pygame.image.load('Graphics/body_horizontal.png').convert_alpha()
+
+        self.body_tr = pygame.image.load('Graphics/body_tr.png').convert_alpha()
+        self.body_tl = pygame.image.load('Graphics/body_tl.png').convert_alpha()
+        self.body_br = pygame.image.load('Graphics/body_br.png').convert_alpha()
+        self.body_bl = pygame.image.load('Graphics/body_bl.png').convert_alpha()
+
     def draw_snake(self):
-        for block in self.body:
-            block_rect = pygame.Rect(block.x * cell_size, block.y * cell_size, cell_size, cell_size)
-            pygame.draw.rect(screen, (183, 111, 122), block_rect)
+        # 7 Позиционирование головы
+        self.update_head_graphics()
+
+        # 4 Все еще нам необходим прямоугольник, чтобы правильно позиционировать спрайт блока
+        for index, block in enumerate(self.body):
+            x_pos = int(block.x * cell_size)
+            y_pos = int(block.y * cell_size)
+            block_rect = pygame.Rect(x_pos, y_pos, cell_size, cell_size)
+
+            # 5 Если голова первый блок, то рисуем голову змеи.
+            if index == 0:
+                screen.blit(self.head, block_rect)
+
+                # screen.blit(self.head_right, block_rect)
+                # 6 В противном случае пока рисуем блок. Однако при запуске можем увидеть, что голова всегда смотрит
+                # в одну сторону, что немного странно, для ее позиционирования создадим отдельный метод
+            else:
+                pygame.draw.rect(screen, (150, 100, 100), block_rect)
+
+        # 3 Чтобы размещать спрайты змеи, нам необходимо понимать в каком положении находится блок перед нами
+        # и после нас.
+
+        # 2 эта часть больше не нужна
+        # for block in self.body:
+        #     block_rect = pygame.Rect(block.x * cell_size, block.y * cell_size, cell_size, cell_size)
+        #     pygame.draw.rect(screen, (183, 111, 122), block_rect)
+
+    def update_head_graphics(self):
+        # 8 Проверяем векторы с направлением движения головы
+        head_relation = self.body[1] - self.body[0]
+        if head_relation == Vector2(1, 0): self.head = self.head_left
+        elif head_relation == Vector2(-1, 0): self.head = self.head_right
+        elif head_relation == Vector2(0, 1): self.head = self.head_up
+        elif head_relation == Vector2(0, -1): self.head = self.head_down
 
     def move_snake(self):
         if self.new_block:
@@ -31,17 +80,13 @@ class SNAKE:
         self.new_block = True
 
 
-
 class FRUIT:
     def __init__(self):
         self.randomize()
 
     def draw_fruit(self):
         fruit_rect = pygame.Rect(self.pos.x * cell_size, self.pos.y * cell_size, cell_size, cell_size)
-        # 7 Отрисовка картинки
-        screen.blit(apple,fruit_rect)
-
-        # pygame.draw.rect(screen, (126, 166, 114), fruit_rect)
+        screen.blit(apple, fruit_rect)
 
     def randomize(self):
         self.x = random.randint(0, cell_number - 1)
@@ -58,7 +103,6 @@ class MAIN:
     def update(self):
         self.snake.move_snake()
         self.check_collision()
-        # 3 Вызов проверки
         self.check_fail()
 
     def draw_elements(self):
@@ -71,25 +115,22 @@ class MAIN:
             self.snake.add_block()
 
     def check_fail(self):
-        # 1 Проверка пересечения границ экрана
         if not 0 <= self.snake.body[0].x < cell_number or not 0 <= self.snake.body[0].y < cell_number:
             self.game_over()
 
-        # 4 проверка пересечения собственного тела
         for block in self.snake.body[1:]:
             if block == self.snake.body[0]:
                 self.game_over()
 
-    # 2 Остановка игры
     def game_over(self):
         pygame.quit()
         sys.exit()
 
+
 pygame.init()
 
 clock = pygame.time.Clock()
-# 7 добавление объекта картинки
-apple = pygame.image.load('graphics/apple.png')
+apple = pygame.image.load('Graphics/apple.png')
 
 cell_size = 40
 cell_number = 20
@@ -111,7 +152,6 @@ while True:
             main_game.update()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                # 6 Запрет змейке двигаться в противоположном направлении
                 if main_game.snake.direction.y != 1:
                     main_game.snake.direction = Vector2(0, -1)
             if event.key == pygame.K_DOWN:
